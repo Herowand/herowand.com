8a43e7932c626c519e206f0c7adc179a05af6ff3
AykutSarac <aykutsarac0@gmail.com>
2023-02-12 13:58:15 +0300

    fix styles dynamic loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import type { AppProps } from "next/app";
 import GlobalStyle from "@/constants/globalStyle";
+import { MantineProvider } from "@mantine/core";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: "dark",
+      }}
+    >
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </MantineProvider>
   );
 }
